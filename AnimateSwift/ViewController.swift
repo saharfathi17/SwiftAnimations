51d82e1faf4ef859067f3dea14d1ddbbf722fba0
Tyler Walker <tyler.walker@dakno.com>
2018-06-21 23:01:20 -0400

    Added pan gesture to view

--- AnimateSwift/ViewController.swift
+++ AnimateSwift/ViewController.swift
@@ -15,6 +15,16 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    @IBAction func handlePan(recognizer: UIPanGestureRecognizer) {
+        let translation = recognizer.translation(in: self.view)
+        
+        if let view = recognizer.view {
+            view.center = CGPoint(x: view.center.x + translation.x, y: view.center.y + translation.y)
+        }
+        
+        recognizer.setTranslation(CGPoint.zero, in: self.view)
+    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
