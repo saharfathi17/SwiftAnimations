e0be1ada8de29442aa742dae84fde35690c5db3c
Tyler Walker <tyler.walker@dakno.com>
2018-06-21 22:32:48 -0400

    Let's move some stuff around

---
+++ AnimateSwift/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  AnimateSwift
+//
+//  Created by Tyler Walker on 6/21/18.
+//  Copyright © 2018 Tyler Walker. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var boxy: UIView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
