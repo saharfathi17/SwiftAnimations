6ebaeb59a718542e54cf94747a6e2a501c391f60
Tyler Walker <tyler.walker@dakno.com>
2018-06-29 20:51:16 -0400

    Toggling the drawerview

--- AnimateSwift/ViewController.swift
+++ AnimateSwift/ViewController.swift
@@ -11,22 +11,40 @@ import UIKit
 class ViewController: UIViewController {
     @IBOutlet var containerView: UIView!
     @IBOutlet weak var drawer: UIView!
+    @IBOutlet weak var boxy: UIView!
+    
+    var drawerIsOpen: Bool = true
     
     @IBAction func animateDrawer(_ sender: Any) {
         print("origin", drawer.bounds.origin)
         print("height", drawer.bounds.height)
         print("size", drawer.bounds.size)
-        let makeNotTheSame = CGAffineTransform(translationX: 0, y: self.containerView.bounds.height)
-        let increment = CGAffineTransform(translationX: 0, y: 100)
-        UIView.animate(withDuration: 1, delay: 0, options: .curveEaseInOut, animations: {
-            self.drawer.transform = increment
-        }, completion: nil)
+        self.drawerIsOpen ? self.hide() : self.reveal()
+        print(self.drawerIsOpen)
 //        UIView.animate(withDuration: 1, delay: 0, usingSpringWithDamping: 0.3, initialSpringVelocity: 0, options: .curveEaseInOut, animations: {
 //            self.drawer.center.y = self.drawer.bounds.height
 //            self.drawer.transform = makeNotTheSame
 //        }, completion: nil)
     }
-    @IBOutlet weak var boxy: UIView!
+    
+    func reveal() {
+        let reveal = CGAffineTransform(translationX: 0, y: 0)
+        UIView.animate(withDuration: 0.4, delay: 0, options: .curveEaseInOut, animations: {
+            self.drawer.transform = reveal
+        }, completion: { (finished: Bool) in
+            self.drawerIsOpen = true
+        })
+    }
+    
+    func hide() {
+        let hide = CGAffineTransform(translationX: 0, y: self.drawer.bounds.height)
+        UIView.animate(withDuration: 0.4, delay: 0, options: .curveEaseInOut, animations: {
+            self.drawer.transform = hide
+        }, completion: { (finished: Bool) in
+            self.drawerIsOpen = false
+        })
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
