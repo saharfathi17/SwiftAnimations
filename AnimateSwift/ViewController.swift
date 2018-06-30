c4e34f66fe1503ad8da85522054363e5d7e93a07
Tyler Walker <tyler.walker@dakno.com>
2018-06-30 17:04:33 -0400

    Handling pan gesture and moving drawer based on translation

--- AnimateSwift/ViewController.swift
+++ AnimateSwift/ViewController.swift
@@ -17,14 +17,7 @@ class ViewController: UIViewController {
     var drawerIsOpen: Bool?
     
     @IBAction func animateDrawer(_ sender: Any) {
-        print("touched")
-        print(self.drawerIsOpen)
         self.drawerIsOpen! ? self.hide() : self.reveal()
-    }
-    
-    @objc func handleOverlayTap(tap: UITapGestureRecognizer) {
-        print("ahdnleing")
-        self.hide()
     }
     
     func reveal() {
@@ -56,16 +49,36 @@ class ViewController: UIViewController {
         
     }
     
+    @objc func handleOverlayTap(tap: UITapGestureRecognizer) {
+        self.hide()
+    }
+    
+    @objc func handleOverlayPan(pan: UIPanGestureRecognizer) {
+        let translation = pan.translation(in: self.overlay)
+        
+        print(translation.y)
+        self.drawer.transform = CGAffineTransform(translationX: 0, y: translation.y * self.drawer.bounds.height)
+    }
+    
+    func addShadowToDrawer() {
+        self.drawer.layer.shadowColor = UIColor.black.cgColor
+        self.drawer.layer.shadowRadius = 5
+        self.drawer.layer.shadowOpacity = 0.5
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.drawerIsOpen = true
+        self.addShadowToDrawer()
         self.overlay.alpha = 0
         let makeFullWidth = CGAffineTransform(scaleX: self.containerView.bounds.width, y: 0)
         self.overlay.transform = makeFullWidth
         self.containerView.addSubview(overlay)
         self.overlay.backgroundColor = UIColor.black
-        let overlayHide = UITapGestureRecognizer(target: self, action: #selector(handleOverlayTap))
-        self.overlay.addGestureRecognizer(overlayHide)
+//        let overlayHide = UITapGestureRecognizer(target: self, action: #selector(handleOverlayTap))
+        let overlayPanSwipe = UIPanGestureRecognizer(target: self, action: #selector(handleOverlayPan))
+        self.containerView.addGestureRecognizer(overlayPanSwipe)
         // Do any additional setup after loading the view, typically from a nib.
     }
     
