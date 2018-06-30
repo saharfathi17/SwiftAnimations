83a61afd3232f446523af8f47926b903c4ebc1ab
Tyler Walker <tyler.walker@dakno.com>
2018-06-30 16:29:32 -0400

    Added tap gesture recognizer

--- AnimateSwift/ViewController.swift
+++ AnimateSwift/ViewController.swift
@@ -17,20 +17,20 @@ class ViewController: UIViewController {
     var drawerIsOpen: Bool = true
     
     @IBAction func animateDrawer(_ sender: Any) {
-        print("origin", drawer.bounds.origin)
-        print("height", drawer.bounds.height)
-        print("size", drawer.bounds.size)
-        self.drawerIsOpen ? self.reveal() : self.hide()
+        self.drawerIsOpen ? self.reveal() : self.hide(sentFrom: "drawer")
     }
     
+    @objc func handleOverlayTap(tap: UITapGestureRecognizer) {
+       print("things")
+    }
     func reveal() {
         let reveal = CGAffineTransform(translationX: 0, y: 0)
         let containerWidth = self.containerView.bounds.width
         let containerHeight = self.containerView.bounds.height
         
         self.containerView.bringSubview(toFront: self.drawer)
+        self.overlay.transform = CGAffineTransform(scaleX: containerWidth, y: containerHeight)
         UIView.animate(withDuration: 0.4, delay: 0, options: .curveEaseInOut, animations: {
-            self.overlay.transform = CGAffineTransform(scaleX: containerWidth, y: containerHeight)
             self.overlay.alpha = 0.2
             self.drawer.transform = reveal
         }, completion: { (finished: Bool) in
@@ -38,17 +38,16 @@ class ViewController: UIViewController {
         })
     }
     
-    func hide() {
+    @objc func hide(sentFrom: String) {
         let hide = CGAffineTransform(translationX: 0, y: self.drawer.bounds.height)
-        let containerWidth = self.containerView.bounds.width
-        let containerHeight = self.containerView.bounds.height
         UIView.animate(withDuration: 0.4, delay: 0, options: .curveEaseInOut, animations: {
             self.overlay.alpha = 0
-            self.overlay.transform = CGAffineTransform(scaleX: containerWidth, y: containerHeight)
             self.drawer.transform = hide
-        }, completion: { (finished: Bool) in
-            self.drawerIsOpen = false
-        })
+        }, completion: nil)
+        
+        let containerWidth = self.containerView.bounds.width
+        self.overlay.transform = CGAffineTransform(scaleX: containerWidth, y: 0)
+        self.drawerIsOpen = false
     }
     
     override func viewDidLoad() {
@@ -58,6 +57,8 @@ class ViewController: UIViewController {
         self.overlay.transform = makeFullWidth
         self.containerView.addSubview(overlay)
         self.overlay.backgroundColor = UIColor.black
+        let overlayHide = UITapGestureRecognizer(target: self, action: #selector(handleOverlayTap))
+        self.overlay.addGestureRecognizer(overlayHide)
         // Do any additional setup after loading the view, typically from a nib.
     }
     
